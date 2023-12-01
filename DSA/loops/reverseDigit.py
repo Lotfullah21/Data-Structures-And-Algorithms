62699d2a459900d9ce17d39f824a847f02cf9345
Lotfullah <85621380+Lotfullah21@users.noreply.github.com>
2023-12-01 22:47:27 +0530

    Add files via upload

---
+++ DSA/loops/reverseDigit.py
@@ -0,0 +1,13 @@
+N = 123456789
+newDigit =  ""
+if (N<0):
+    N = N * -1
+while(N>0):
+    # it will always gives the last digit as a remainder
+    lastDigit = N%10
+    newDigit = str(newDigit) + str(lastDigit) 
+    # int(N/10) removes the last digit which returns only int digits, and the last digit will be on the other side of the decimal
+    N = int(N/10)
+
+
+print("new digit",newDigit)
