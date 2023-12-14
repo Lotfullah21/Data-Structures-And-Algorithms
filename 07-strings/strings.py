0a3bd87a34df85e0bcbc9e84dcaf253f1ff72fbf
Lotfullah <85621380+Lotfullah21@users.noreply.github.com>
2023-12-14 23:59:26 +0530

    Add files via upload

--- 07-strings/strings.py
+++ 07-strings/strings.py
@@ -16,4 +16,42 @@ charSummation = ord(char1) + ord(char2)
 print(f"{char1} + {char2} =",charSummation)
 
 
-# we can use str() to 
+# we can use chr() to obtain the ASCII character of an integer.
+b = chr(90)
+print("ASCII character of 90 is",b)
+
+
+name = "HoshmandLab"
+
+print(name[0])
+print(name[-1:])
+print(name[0:])
+
+
+
+my_string = "This is a test string!"
+# split method will be create substrings based on white space, ["This", "is","a","test","string"]
+substring = my_string.split() # This
+substring0 = my_string.split()[0] # This
+print(substring0)
+print(substring[0])
+
+
+my_string_slice = my_string[0:-1]
+print(my_string_slice)
+
+# find method is used to get the index related to that particular string.
+my_string_this_index = my_string.find("is")
+print("index of is = ",my_string_this_index)
+
+
+# length of a string, it counts number of characters and white spaces as well.
+print("length of my_string",len(my_string))
+# if we index for a particular number, we will be having only that particular character not the whole word.
+print(my_string[0])
+
+
+
+print(chr(ord("v")+9))
+
+
