0cda8d49580068fa8b7d3533a785e84370801b31
lotfullah andishmand <lotfullahandishmand@lotfullahs-MacBook-Air.local>
2024-01-15 15:56:37 +0530

    README file added

--- 15-linked-list/deleteLast.py
+++ 15-linked-list/deleteLast.py
@@ -10,7 +10,9 @@ def printList(head: Node) -> Node:
         curr = curr.next
         
 def deleteLastNode(head: Node) -> Node:
-    if head.next is not None:
+    if head is not None and head.next is not None:
+        # Here, temp and head are both references pointing to the same linked list. 
+        # we use to temp to traverse. what changes happened caused by this will be reflected in original linkedList (head)
         temp = head
         # the 2nd last is node the one that 2ndLast.next.next == None.
         while(temp.next.next!=None):
@@ -18,7 +20,7 @@ def deleteLastNode(head: Node) -> Node:
         # once found the 2nd last, cut the tail and put the next to the none value instead of last.
         temp.next = None
         # return the updated head
-        return head 
+        return head
     print("HEAD IS NULL")
 
 head = Node(10)
@@ -32,6 +34,6 @@ temp2.next = temp3
 
 print("Original linked list")
 printList(head)
-head = deleteLastNode(head)
+newList = deleteLastNode(head)
 print("\nLinked list after deleting the last node")
-printList(head)
+printList(newList)
