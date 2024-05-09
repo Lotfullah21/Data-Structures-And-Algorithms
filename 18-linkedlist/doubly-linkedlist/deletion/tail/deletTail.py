0bd352b3a2101cd230f41b968754df355b670c61
Lotfullah <andishmandlotfullah21@gmail.com>
2024-05-09 20:18:56 +0530

    Operations in doubly linked list implemented

--- 18-linkedlist/doubly-linkedlist/deletion/tail/deletTail.py
+++ 18-linkedlist/doubly-linkedlist/deletion/tail/deletTail.py
@@ -4,6 +4,22 @@ class Node:
         self.prev = None
         self.next = None
 
+
+def deleteTail(head: Node) -> Node:
+    "Deletes the last node from the linked list and return a new linked list."
+    if head is None or head.next is None:
+        return None
+    curr = head
+    # Iterate up to the last node
+    while curr.next!=None:
+        curr = curr.next
+    # Find the previous to last node
+    secondLastNode = curr.prev
+    # Break the chain and make next of last 2nd node to None
+    secondLastNode.next = None
+    return head
+
+
 def insertInTail(head,data):
     #code here
     newNode = Node(data)
@@ -32,11 +48,14 @@ def displayList(head: Node) -> list:
 
 # Example usage
 head = Node(2)
+insertInTail(head, 4)
+insertInTail(head, 6)
+insertInTail(head, 8)
 insertInTail(head, 10)
-insertInTail(head, 10)
-insertInTail(head, 10)
-insertInTail(head, 10)
-insertInTail(head, 10)
-insertInTail(head, 10)
+insertInTail(head, 12)
+insertInTail(head, 14)
 ans = displayList(head)
-print(ans)
+print("Original linked list",ans)
+newLinkedList = deleteTail(head)
+ans = displayList(newLinkedList)
+print("Linked list after tail's node deletion",ans )
