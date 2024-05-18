bb2f40c23aa0a60a0d9b945f8cf9fea6a21a14ae
Lotfullah <andishmandlotfullah21@gmail.com>
2024-05-18 08:47:41 +0530

    Depth first traversal in trees added

--- 18-trees/depthFristTraversal/preOrder/preOrder.py
+++ 18-trees/depthFristTraversal/preOrder/preOrder.py
@@ -1,31 +1,3 @@
-"""
-Given a binary tree, find its preorder traversal.
-
-Example 1:
-
-Input:
-        1      
-      /          
-    4    
-  /    \   
-4       2
-Output: 1 4 4 2 
-
-
-Input:
-       6
-     /   \
-    3     2
-     \   / 
-      1 2
-Output: 6 3 1 2 2 
-
-
-You just have to complete the function preOrder() which takes the root node of the tree as input and returns an array containing the preOrder traversal of the tree.
-
-"""
-
-
 # Node Class:
 class Node:
     def __init__(self,val):
@@ -35,14 +7,22 @@ class Node:
 
 #Function to return a list containing the preOrder traversal of the tree. 
 class Solution:
-    def PreOrder(self,root):
-        preorder = []
-        if root is not None:
-            preorder.append(root.data)
-            preorder.extend(self.PreOrder(root.left))
-            preorder.extend(self.PreOrder(root.right))
-        # print(preorder)
-        return preorder
+    def mainPreOrder(self,root):
+      result = []
+      self.preOrder(root, result)
+      return result
+      
+      
+    def preOrder(self, root, result):
+      if root==None:
+        return
+      else:
+        result.append(root.data)
+        self.preOrder(root.left, result)
+        self.preOrder(root.right, result)
+        
+        
+        
     
 #        1
 #       / \
@@ -61,7 +41,7 @@ root.left.right = Node(5)
 solution = Solution()
 
 # Get the in-order traversal
-pre_result = solution.PreOrder(root)
+preOrderResult = solution.mainPreOrder(root)
 
 # Print the result
-print("Pre-order traversal:", pre_result)
+print("Pre-order traversal:", preOrderResult)
